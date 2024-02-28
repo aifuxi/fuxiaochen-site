16e31294a2ad4ceb2d8da9966f1790c20aed0f2a
fuxiaochen <aifuxi.js@gmail.com>
2024-02-28 23:14:33 +0800

    feat: 🎸 完善首页内容

--- app/page.tsx
+++ app/page.tsx
@@ -1,12 +1,66 @@
-import { NICKNAME } from '@/constants/info';
+import Link from 'next/link';
+
+import {
+  SolarArrowRightBoldDuotone,
+  SolarBook2BoldDuotone,
+  SolarLetterBoldDuotone,
+} from '@/components/icons';
+
+import { EMAIL, NICKNAME } from '@/constants/info';
 
 export default function Home() {
   return (
     <div className="flex flex-col items-center pt-48">
-      <div className="max-w-screen-md">
-        <p className="text-4xl md:text-6xl leading-[1.125] font-bold text-center tracking-normal">
-          Hi~ 我是{NICKNAME} 👋
+      <div className="max-w-screen-md flex flex-col gap-y-6">
+        <p className="text-4xl md:text-6xl leading-[1.125] font-bold text-center tracking-normal flex">
+          Hi~ 我是
+          <p
+            style={{
+              background:
+                'linear-gradient(to right, rgb(191, 228, 255), rgb(139, 117, 226)) text',
+              WebkitTextFillColor: 'transparent',
+            }}
+          >
+            {NICKNAME}
+          </p>
+          👋
         </p>
+        <div className="text-xl md:text-2xl text-zinc-300/90 mb-5 flex flex-col gap-2">
+          <p>欢迎来到我的个人网站！</p>
+          <p>
+            我在这里写 Blog 记录和分享我的成长，希望我的 Blog
+            能为你提供有价值的信息，帮助你解决技术问题，或者给你带来一些灵感。
+          </p>
+          <p>
+            如果你有任何问题、建议或想法，欢迎发邮件联系我，我看到后会尽快回复
+            😘。
+          </p>
+        </div>
+        <div className="flex gap-x-8">
+          <Link
+            href="/blog"
+            className="inline-flex flex-row justify-center items-center  text-lg  text-slate-50 text-center px-5 rounded-full font-medium transition py-3"
+            style={{
+              background:
+                'linear-gradient(45deg, rgb(36, 91, 202), rgb(111, 68, 191))',
+            }}
+          >
+            看看 Blog <SolarBook2BoldDuotone className="text-lg ml-2" />
+          </Link>
+          <Link
+            href={`mailto:${EMAIL}`}
+            className="inline-flex flex-row justify-center items-center  text-lg  text-slate-50 text-center px-5 rounded-full font-medium transition py-3 bg-zinc-900  ring-1 ring-zinc-800 "
+          >
+            发邮件给这小子 <SolarLetterBoldDuotone className="text-lg ml-2" />
+          </Link>
+          <Link
+            className="inline-flex flex-row gap-2 justify-center items-center text-lg font-medium text-slate-200 text-center px-5 rounded-full transition py-3 hover:text-white"
+            href="/about"
+          >
+            <span>关于</span>
+            <SolarArrowRightBoldDuotone className="text-lg" />
+          </Link>
+        </div>
       </div>
     </div>
   );
--- constants/info.ts
+++ constants/info.ts
@@ -1,3 +1,5 @@
 export const NICKNAME = '付小晨';
 
 export const SLOGAN = '先模仿，再超越，努力做一个更好的程序员。';
+
+export const EMAIL = 'aifuxi.js@gmail.com';
