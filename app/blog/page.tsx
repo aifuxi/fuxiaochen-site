ad1cfa859226c9cae02df05b20d26ba652b6b969
fuxiaochen <aifuxi.js@gmail.com>
2024-02-29 22:07:59 +0800

    feat: 🎸 文章列表加上封面

--- app/blog/page.tsx
+++ app/blog/page.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 
 import { type Metadata } from 'next';
+import Image from 'next/image';
 import Link from 'next/link';
 
 import { format, parseISO } from 'date-fns';
+
+import PostTag from '@/components/post-tag';
 
 import { env } from '@/libs/env.mjs';
 
@@ -12,6 +15,8 @@ import { type Post, type ResponseTotalStruct } from '@/types';
 export const metadata: Metadata = {
   title: 'Blog',
 };
+
+export const revalidate = 60;
 
 export default async function Page() {
   const res = await fetch(`${env.BASE_URL}/posts`);
@@ -27,21 +32,39 @@ export default async function Page() {
         </p>
         <div className="gap-4 flex flex-col">
           {posts.map((el) => (
-            <Link key={el.id} href={`/blog/${el.slug}`}>
-              <div className="rounded-2xl border border-slate-800 bg-slate-950 text-slate-50 shadow-sm">
-                <div className="flex flex-col space-y-1.5 p-6">
+            <Link
+              key={el.id}
+              href={`/blog/${el.slug}`}
+              className="rounded-2xl border border-slate-800 bg-slate-950 text-slate-50 shadow-sm flex items-center p-6"
+            >
+              <div className="flex-1 flex flex-col">
+                <div className="flex flex-col space-y-1.5">
                   <h3 className="text-2xl font-semibold leading-none tracking-tight">
                     {el.title}
                   </h3>
-                  <p className="text-sm text-slate-400">{el.desc}</p>
+                  <p className="text-sm text-slate-400 pt-2">{el.desc}</p>
                 </div>
-                <div className="flex items-center p-6 pt-0">
-                  <span className="text-sm text-slate-350">
-                    {el.author}&nbsp;·&nbsp;
-                    {format(parseISO(el.createdAt), 'MMMM dd, yyyy')}
-                  </span>
+                <div className="flex flex-col gap-y-4 pt-6">
+                  <div className="text-sm text-slate-350 flex">
+                    <div className="flex-1">
+                      {el.author}&nbsp;·&nbsp;
+                      {format(parseISO(el.createdAt), 'MMMM dd, yyyy')}
+                    </div>
+                  </div>
+                  <div className="flex flex-row gap-2">
+                    {el.tags?.map((tag) => <PostTag key={tag.id} tag={tag} />)}
+                  </div>
                 </div>
               </div>
+              {el.cover && (
+                <Image
+                  src={el.cover}
+                  width={110}
+                  height={74}
+                  className="ml-6"
+                  alt={el.title}
+                />
+              )}
             </Link>
           ))}
         </div>
